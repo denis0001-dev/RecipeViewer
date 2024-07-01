120185fdd74d23c32a5fa4ba834e488db3486f91
denis0001-dev <demorozov2048@gmail.com>
2024-07-01 15:03:51 +0300

    Revert "Веб-версия: Исправление перенаправления 2"
    This reverts commit f0c336d401ab5fdd4f5a12be4b94edd62de20ae0.

--- web/redirect.js
+++ web/redirect.js
@@ -1,3 +1,3 @@
 if (parent === window) {
-    location.href = `./index.html?page=${document.head.querySelector("title").innerHTML.toLowerCase()}`;
+    location.href = `../index.html?page=${document.head.querySelector("title").innerHTML.toLowerCase()}`;
 }
